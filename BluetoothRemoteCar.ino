37cd05d729db1ab0ac7357a9eba29a7b13754e1e
jim60105 <jim60105@gmail.com>
2018-01-18 02:06:20 +0800

    Move Smoothly TODO: Loop by seperate delay

--- BluetoothRemoteCar.ino
+++ BluetoothRemoteCar.ino
@@ -1,37 +1,65 @@
 #include  <SoftwareSerial.h>
 SoftwareSerial BTSerial(10, 11); // RX | TX
-#define INPUT_SIZE 8
 
 // defines pins numbers
-const int stepPin = 3; 
-const int dirPin = 4; 
-const int stepPin2 = 5; 
-const int dirPin2 = 6; 
+const int stepPinR = 3; //Right
+const int dirPinR = 4; 
+const int stepPinL = 5; //Left
+const int dirPinL = 6; 
 
-void run(int roll, int pitch){
-  Serial.print(roll);
-  Serial.print('@');
-  Serial.println(pitch);
-  if(pitch<0){
-    digitalWrite(dirPin,LOW); // Enables the motor to move in a particular direction
-    digitalWrite(dirPin2,LOW); // Enables the motor to move in a particular direction
-    pitch = abs(pitch);
-  }else{
-    digitalWrite(dirPin,HIGH); // Enables the motor to move in a particular direction
-    digitalWrite(dirPin2,HIGH); // Enables the motor to move in a particular direction
+String roll,pitch;
+int int_roll,int_pitch,loopTimes;
+int int_roll_old=3000,int_pitch_old=3000;
+int LDelay,RDelay;
+void run(int LDelay, int RDelay){
+
+  if(LDelay<0){ //Backward
+    digitalWrite(dirPinL,LOW); // Enables the motor to move in a particular direction
+    LDelay = abs(LDelay);
+  }else{ //Forward
+    digitalWrite(dirPinL,HIGH); // Enables the motor to move in a particular direction
   }
-  // Makes 200 pulses for making one full cycle rotation
-  for(int x = 0; x < 18; x++) {
+  if(RDelay<0){ //Backward
+    digitalWrite(dirPinR,LOW); // Enables the motor to move in a particular direction
+    RDelay = abs(RDelay);
+  }else{ //Forward
+    digitalWrite(dirPinR,HIGH); // Enables the motor to move in a particular direction
+  }
+
+  loopTimes = (LDelay+RDelay)/(-112)+70;
+  
+      Serial.print(':');
+      Serial.println(loopTimes);
+  // 200 pulses for making one full cycle rotation
+  //TODO 按照比例loop
+  for(int x = 0; x < loopTimes; x++) {
     
-    digitalWrite(stepPin,HIGH); 
-    delayMicroseconds(pitch); 
-    digitalWrite(stepPin,LOW); 
-    delayMicroseconds(pitch); 
-    digitalWrite(stepPin2,HIGH); 
-    delayMicroseconds(pitch); 
-    digitalWrite(stepPin2,LOW);
-    delayMicroseconds(pitch); 
+    digitalWrite(stepPinR,HIGH); 
+    delayMicroseconds(RDelay); 
+    digitalWrite(stepPinR,LOW); 
+    delayMicroseconds(RDelay); 
+    digitalWrite(stepPinL,HIGH); 
+    delayMicroseconds(LDelay); 
+    digitalWrite(stepPinL,LOW);
+    delayMicroseconds(LDelay); 
   }
+}
+
+int checkBound(int i){
+  if(i>=0){
+    if (i>3000){
+      i = 3000;
+    }else if(i<1000){
+      i = 1000;
+    }
+  }else{
+    if (i<-3000){
+      i = -3000;
+    }else if (LDelay>-1000){
+      i = -1000;
+    }
+  }
+  return i;
 }
 
 void setup()
@@ -47,40 +75,61 @@ void setup()
   BTSerial.setTimeout(10);
   
   // Sets the two pins as Outputs
-  pinMode(stepPin,OUTPUT); 
-  pinMode(dirPin,OUTPUT);
-  pinMode(stepPin2,OUTPUT); 
-  pinMode(dirPin2,OUTPUT);
+  pinMode(stepPinR,OUTPUT); 
+  pinMode(dirPinR,OUTPUT);
+  pinMode(stepPinL,OUTPUT); 
+  pinMode(dirPinL,OUTPUT);
 }
-
-String roll,pitch;
-int int_roll,int_pitch;
-int int_roll_old=3000,int_pitch_old=3000;
 
 void loop() 
 {
   // Keep reading from HC-05 and send to Arduino Serial Monitor
   if (BTSerial.available()){
+    BTSerial.readStringUntil('[');
     roll = BTSerial.readStringUntil(',');
     if(roll != ""){
-      pitch = BTSerial.readStringUntil(',');
+      pitch = BTSerial.readStringUntil(']');
       int_roll=roll.toInt();
       int_pitch=pitch.toInt();
-
       
       if(int_pitch>0){
-        int_pitch = (-200/9)*int_pitch+3000;
-        int_pitch_old = (int_pitch*3+int_pitch_old)/4;
-        if(int_pitch_old<3000 && int_pitch_old>1000){
-          run(int_roll,int_pitch_old);
-        }
+        int_pitch = (-200/7)*int_pitch+3000;
       }else{
-        int_pitch = (-200/9)*int_pitch-3000;
-        int_pitch_old = (int_pitch*3+int_pitch_old)/4;
-        if(int_pitch_old>-3000 && int_pitch_old<-1000){
-          run(int_roll,int_pitch_old); 
-        }
+        int_pitch = (-200/7)*int_pitch-3000;
       }
+      Serial.print(int_roll);
+      Serial.print('@');
+      int_roll = int_roll*42;
+      Serial.print(int_roll);
+      Serial.print('@');
+      int_pitch_old = (int_pitch*3+int_pitch_old)/4;
+      int_roll_old = (int_roll*3+int_roll_old)/4;
+      Serial.print(int_roll);
+      Serial.print('@');
+      if(int_roll_old>0){
+        LDelay = int_pitch_old-int_roll_old;
+        RDelay = int_pitch_old+int_roll_old;
+      }else{
+        LDelay = int_pitch_old+int_roll_old;
+        RDelay = int_pitch_old-int_roll_old;
+      }Serial.print(int_roll);
+      Serial.print('@');
+      LDelay = checkBound(LDelay);
+      RDelay = checkBound(RDelay);
+      
+      Serial.print(int_roll);
+      Serial.print('@');
+      Serial.print(int_pitch);
+      Serial.print("  =>  ");
+      Serial.print(int_roll_old);
+      Serial.print('@');
+      Serial.print(int_pitch_old);
+      Serial.print("  =>  ");
+      Serial.print(LDelay);
+      Serial.print('@');
+      Serial.print(RDelay);
+      run(LDelay,RDelay);
+     
     }
   }
   // Keep reading from Arduino Serial Monitor and send to HC-05
